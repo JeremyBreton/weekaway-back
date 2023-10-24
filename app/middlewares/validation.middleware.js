3414298181ea81ae13cac95e844e007525269716
Geoffrey Fardeau <fardeau.geoffrey@gmail.com>
2023-10-24 12:01:32 +0200

    :tada: Creating tree structure

---
+++ app/middlewares/validation.middleware.js
@@ -0,0 +1,16 @@
+/*
+In this schema : schemaGet ou schemaPost
+in dataSource : 'query' ou 'body'
+*/
+
+// 
+export default (schema, dataSource) => async (req, res, next) => {
+  try {
+    // req['query'] ou req['body']
+    // same as à req.query ou req.body
+    await schema.validateAsync(req[dataSource]);
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
