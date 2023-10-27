f1939213684619f8193eefe31b7c63fdfc7c05ad
Renaud-Berni <renaud.berni@oclock.school>
2023-10-27 10:44:05 +0200

    Swagger to finish/HTTP request organised

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ import './app/helpers/env.load.js';
 import { createServer } from 'node:http';
 // L'application sera récupérer en tant que module
 import app from './app/index.app.js';
-
-import userDocImplementation from './app/middlewares/swagger.doc.js';
-
-userDocImplementation(app);
-
 // Et on l'injecte dans le serveur web, comme on fournirai un middleware pour gérer les requêtes.
 const server = createServer(app);
 
