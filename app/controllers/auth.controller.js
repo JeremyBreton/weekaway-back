852b539adbc64e0afef977075376b989bd4d4619
Renaud-Berni <renaud.berni@oclock.school>
2023-10-27 14:17:58 +0200

    :bug: Fix-Register

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -81,7 +81,7 @@ export default {
           message: 'Inscription réussie!',
           firstname: registeredUser.firstname,
           logged: true,
-          tokeb: token,
+          token,
         });
     }
     return res.status(500).json({ message: 'Erreur lors de l’inscription.', logged: false });
