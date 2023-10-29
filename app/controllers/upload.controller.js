ff8eb1f5a6384f8abfff7c769aab33fe85b748a0
Geoffrey Fardeau <fardeau.geoffrey@gmail.com>
2023-10-29 10:21:14 +0100

    API launch on VPS

--- app/controllers/upload.controller.js
+++ app/controllers/upload.controller.js
@@ -1,11 +1,10 @@
-
 import userDataMapper from '../models/user.dataMapper.js';
 import eventDataMapper from '../models/event.dataMapper.js';
 
 export default {
 
   uploadProfilePicture(req, res) {
-    const path = `http://localhost:3001/static/${req.file.filename}`;
+    const path = `http://caca-boudin.fr/static/${req.file.filename}`;
     // replace by id received by the front
     userDataMapper.updateUserPic(22, path);
 
@@ -13,7 +12,7 @@ export default {
   },
 
   uploadEventPicture(req, res) {
-    const path = `http://localhost:3001/static/${req.file.filename}`;
+    const path = `http://caca-boudin.fr/static/${req.file.filename}`;
     // replace by id received by the front
     eventDataMapper.updateEventPicture(1, path);
     res.json({ message: 'image uploadée' });
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import app from './app/index.app.js';
 // Et on l'injecte dans le serveur web, comme on fournirai un middleware pour gérer les requêtes.
 const server = createServer(app);
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`API launched on http://localhost:${PORT}`));
+if (process.env.NODE_ENV === 'development') {
+  server.listen(3000, () => console.log(`API launched on http://localhost:${3000}`));
+} else if (process.env.NODE_ENV === 'production') {
+  server.listen(80, () => console.log(`API launched on http://localhost:${80}`));
+}
