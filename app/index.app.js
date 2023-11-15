fa29c5c3777f1a32a105de67f0ecc334d3b8d386
Geoffrey Fardeau <fardeau.geoffrey@gmail.com>
2023-11-15 14:48:20 +0100

    :bug: hotfix cors

--- app/index.app.js
+++ app/index.app.js
@@ -38,6 +38,13 @@ const corsOptions = {
 };
 */
 app.use(cors());
+
+app.all('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  // ...
+});
 app.use(cookieParser());
 // Middleware pour récupérer un body au format JSON
 app.use(express.json());
