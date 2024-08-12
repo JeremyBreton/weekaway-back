475f67282e9bec5e6333ea895a46b155e32843ed
Jérémy <breton.jeremy@live.fr>
2024-08-12 12:17:21 +0200

    Fix port configuration and environment settings

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ const server = createServer(app);
 
 // Production mode or dev mode
 
-if (process.env.NODE_ENV === 'development') {
-  server.listen(process.env.PORT || 3000, () => console.log(`API launched on http://localhost:${3000}`));
-} else if (process.env.NODE_ENV === 'production') {
-  server.listen(80, () => console.log(`API launched on http://localhost:${80}`));
-}
+// if (process.env.NODE_ENV === 'development') {
+//   server.listen(process.env.PORT || 3000, () => console.log(`API launched on http://localhost:${3000}`));
+// } else if (process.env.NODE_ENV === 'production') {
+//   server.listen(80, () => console.log(`API launched on http://localhost:${80}`));
+// }
+
+// Utilisation du port attribué par Heroku
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`API launched on http://localhost:${PORT}`);
+});
